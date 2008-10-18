8a1de7a3f5e10586e39315c6a0e6522d614e6911
mihasya <m@mihasya.com>
2008-10-18 13:05:55 -0700

    added apc handler; refactored cacheHdl and fileCacheHdl

--- mihasya_cache.php
+++ mihasya_cache.php
@@ -1,68 +1,73 @@
 <?php
 abstract class CacheNamespace {
-  public static $handlers;
-  public static $conf = array();
+    public static $handlers;
+    public static $conf = array();
 }
 
 function cache($hdlName) {
-  if (!CacheNamespace::$handlers[$hdlName]) {
-    if (!CacheNamespace::$conf[$hdlName]) return false; //init hasnt been run
-    //go on and create the connection
-    $conf = CacheNamespace::$conf[$hdlName];
-    $className = $conf['type'].'CacheHdl';
-    $hdl = new $className($conf['initArgs']);
-    CacheNamespace::$handlers[$hdlName] = $hdl;
-  }
-  return CacheNamespace::$handlers[$hdlName];
+    if (!CacheNamespace::$handlers[$hdlName]) {
+        if (!CacheNamespace::$conf[$hdlName]) return false; //init hasnt been run
+        //go on and create the connection
+        $conf = CacheNamespace::$conf[$hdlName];
+        $className = $conf['type'].'CacheHdl';
+        $hdl = new $className($conf['initArgs']);
+        CacheNamespace::$handlers[$hdlName] = $hdl;
+    }
+    return CacheNamespace::$handlers[$hdlName];
 }
 
 function cache_init($hdlName, $type='file', $initArgs = array()) {
-  $conf = array(
-    'type'=>$type,
-    'initArgs'=>$initArgs
-  );
-  CacheNamespace::$conf[$hdlName] = $conf;
+    $conf = array(
+        'type'=>$type,
+        'initArgs'=>$initArgs
+    );
+    CacheNamespace::$conf[$hdlName] = $conf;
 }
 
 abstract class cacheHdl {
-  //TODO: add initArgs validation to __construct; iterate over $this->_requiredArgs 
-  //and set those in the handler class definitions
-  abstract function __construct($initArgs);
-  abstract function get($key);
-  abstract function set($key, $value, $ttl=null);
+    //TODO: add initArgs validation to __construct; iterate over $this->_requiredArgs
+    //and set those in the handler class definitions
+    abstract function __construct($initArgs);
+    abstract function get($key);
+    abstract function set($key, $value, $ttl=null);
+    protected function _getRealKey($key) {
+        //TODO: store the key so that we don't have to run m5 every time
+        //TODO: make this modifiable so that the user can define own hash/salt
+        return md5($key);
+    }
 }
 
-class fileCacheHdl extends cacheHdl{
-  function __construct($initArgs) {
-    $this->path = $initArgs['path'];
-  }
-  //break this out into its own function incase I need to hash differently
-  function getRealKey($key) {
-    return md5($key);
-  }
-  function get($key) {
-    $realKey = $this->getRealKey($key);
-    $path = $this->path.'/'.$realKey;
-    if (!file_exists($path)) return false;
-    $contents = file_get_contents($path);
-    $matches = array();
-    $m = preg_match("/([0-9]+),/",$contents, $matches);
-    if ($matches[1]==0 || $matches[1] < time()) {
-      //TODO: actually strip out the ttl and return the value
-      echo $contents;
-    } else {
-      unlink($path);
-      return false;
+class apcCacheHdl extends cacheHdl{
+    function __construct($initArgs) {
+        //TODO: maybe set a default ttl?
+        if (!function_exists('apc_fetch')) {
+            throw new Exception ("APC does not appear to be enabled");
+        }
     }
-  }
-  function set($key, $value, $ttl=null) {
-    $realKey = $this->getRealKey($key);
-    $ttl = $ttl ? time()+$ttl : 0;
-    $toStore = is_string($value) ? $value : serialize($value);
-    $toStore = $ttl.','.$toStore;
-    $f = fopen($this->path.'/'.$realKey, 'w');
-    fwrite($f, $toStore);
-    fclose($f);
-  }
+    function get($key) {
+        return apc_fetch($this->_getRealKey($key));
+    }
+    function set($key, $value, $ttl=null) {
+        return apc_store($this->_getRealKey($key), $value, $ttl);
+    }
+}
+class fileCacheHdl extends cacheHdl{
+    private $_path;
+    private $_ttl;
+    function __construct($initArgs) {
+        //TODO: check for empty fields
+        $this->_path = $initArgs['path'];
+        $this->_ttl = $initArgs['ttl'];
+    }
+    function get($key) {
+        //TODO: use $this->ttl and file's mtime to check if it's expired
+    }
+    function set($key, $value, $ttl=null) {
+        $realKey = $this->_getRealKey($key);
+        $ttl = $ttl ? time()+$ttl : 0;
+        $f = fopen($this->path.'/'.$realKey, 'w');
+        fwrite($f, $value);
+        fclose($f);
+    }
 }
 ?>
